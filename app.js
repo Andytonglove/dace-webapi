18ea652b84c107b6bd7b0981c65a16fab9ef481c
ParatrooperAndy <1468492471@qq.com>
2022-12-11 22:51:39 +0800

    update todos

--- app.js
+++ app.js
@@ -14,32 +14,39 @@ function isValidId(id) {
     return /^\d{8,13}$/.test(id);
 }
 
-// 对应上面的API，提供APP端的命令行操作，这里命令行操作实现可以直接调用上面的API
+// 对应上面的API，提供APP端的命令行操作，这里命令行操作实现可以直接调用上面的API，
 // 长、短项：单「-」代表短选项，双「--」代表长选项。长短项表示的效果相同，可随意选择使⽤
-// 联合选项：-u id {-n｜-i|-m|-b}, 即-u选项必须附带最少后⾯的四个选项之⼀才
-// 能工作，否则警告提示 - a { -n - i - m - b }, 即 - a选项必须同时附带后面的4个选项才能工作，否则警告提示
+// 联合选项：-u id {-n｜-i|-m|-b}, 即-u选项必须附带最少后面的四个选项之⼀才能工作，否则警告提示
+// 同理，- a { -n - i - m - b }, 即 - a选项必须同时附带后面的4个选项才能工作，否则警告提示
+// 互斥选项：-a | -d | -u | -l ,即该4个选项只能同时选择⼀个。如果选择多个，警告提示
+// 以上选项，均为非强制选项（即：如果运行时无选项，则按照「 --list=descend」操作（因为这是⼀个安全操作）
+
+
+// command和option的区别：command是命令，option是选项
+// 用option的方式实现上面的功能，问题是在输入了正确参数的情况下仍报错请输入正确的参数，为什么
 program
     .version('0.0.1')
     .description('An app of Personal Information Collection System')
     .option('-a, --add', 'add a new item')
     .option('-u, --update', 'update an item')
     .option('-d, --delete <id>', 'delete an item')
-    .option('-l, --list [sort]', 'list all items')
+    .option('-l, --list [sort]', 'list all items', 'descend')
+    .option('-f, --find <id>', 'find an item')
     .option('-n, --name <name>', 'add a new name')
     .option('-i, --id <id>', 'add a new id')
     .option('-m, --mobile <mobile>', 'add a new mobile')
     .option('-b, --hobby <hobby>', 'add a new hobby')
     .option('-e, --email <email>', 'add a new email')
-    .option('-f, --find <id>', 'find an item')
     .parse(process.argv);
 
-// 当选择-a和-u选项时，提示用户不能同时选择
-if (program.add && program.update) {
-    console.log('不能同时选择-a和-u选项');
+
+// 互斥选项：-a | -d | -u | -l ,即该4个选项只能同时选择⼀个。如果选择多个，警告提示
+if (program.add + program.delete + program.update + program.list > 1) {
+    console.log('互斥选项：-a | -d | -u | -l ,即该4个选项只能同时选择⼀个');
     process.exit(1);
 }
 
-// 当选择-a选项时，必须输入--name、--id、--mobile、--hobby、--email参数，支持短参数和长参数
+// 联合选项：当选择-a选项时，必须输入--name、--id、--mobile、--hobby、--email参数，支持短参数和长参数
 if (program.add) {
     if (!program.name || !program.id || !program.mobile || !program.hobby || !program.email) {
         console.log('添加条目时，必须输入--name、--id、--mobile、--hobby、--email参数');
@@ -47,19 +54,24 @@ if (program.add) {
     }
 }
 
-// 当选择-u选项时，必须输入--id参数，支持短参数和长参数
+// 联合选项：当选择-u选项时，必须输入--id参数，支持短参数和长参数，还需要至少输入一个更新参数
 if (program.update) {
     if (!program.id) {
         console.log('更新条目时，必须输入--id参数');
+        process.exit(1);
+    }
+    if (!program.name && !program.mobile && !program.hobby && !program.email) {
+        console.log('更新条目时，必须输入至少一个更新参数');
         process.exit(1);
     }
 }
 
 // 示例：node app -a -n "张三" -i "1234567890" -m "17345678901" -b "篮球" -e "zhangsan@example.com"
 
-// 如果没有输入命令，则默认输出帮助信息
+// 如果没有输入命令，则默认输出帮助信息，TODO：也可按照「 --list=descend」操作
 if (!process.argv.slice(2).length) {
     program.outputHelp();
+    process.exit(1);
 }
 
 // 如果输入了命令，执行对应的命令
@@ -70,11 +82,15 @@ if (program.add) {
     var mobile = program.mobile;
     var hobby = program.hobby;
     var email = program.email;
-    // 如果没有输入参数，提示用户输入参数
-    if (!name || !id || !mobile || !hobby || !email) {
-        console.log('请输入参数');
+
+    console.log('添加条目：' + name + ' ' + id + ' ' + mobile + ' ' + hobby + ' ' + email);
+
+    // 参数校验，如果参数不合法，则直接退出
+    if (!isValidId(id) || !isValidMobile(mobile) || !isValidEmail(email)) {
+        console.log('参数不合法');
         return;
     }
+
     // 使用request模块发送POST请求，添加条目，调用上面服务端的API：
     request.post({
         url: 'http://localhost:1337/api/add',
@@ -100,11 +116,6 @@ if (program.add) {
     var mobile = program.mobile;
     var hobby = program.hobby;
     var email = program.email;
-    // 如果没有输入参数，提示用户输入参数
-    if (!name || !id || !mobile || !hobby || !email) {
-        console.log('请输入参数');
-        return;
-    }
     if (!isValidId(id) || !isValidMobile(mobile) || !isValidEmail(email)) {
         console.log('参数不合法');
         return;
@@ -152,17 +163,8 @@ if (program.add) {
 } else if (program.list) {
     // 调用上面服务端的API：api/list，列出所有条目
     var sort = program.sort;
-    // 如果没有输入参数，按照原始顺序列出所有条目
-    if (!sort) {
-        // 使用request模块发送GET请求，列出所有条目
-        request.get('http://localhost:1337/api/list', function (err, httpResponse, body) {
-            if (err) {
-                console.log('列出失败');
-            } else {
-                console.log(body);
-            }
-        });
-    } else if (sort === 'asc') {
+    // 如果没有输入参数，按照des列出所有条目
+    if (sort === 'ascend') {
         // 使用request模块发送GET请求，列出所有条目
         request.get('http://localhost:1337/api/list?sort=asc', function (err, httpResponse, body) {
             if (err) {
@@ -171,7 +173,7 @@ if (program.add) {
                 console.log(body);
             }
         });
-    } else if (sort === 'desc') {
+    } else if (sort === 'descend') {
         // 使用request模块发送GET请求，列出所有条目
         request.get('http://localhost:1337/api/list?sort=desc', function (err, httpResponse, body) {
             if (err) {
